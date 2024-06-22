8039be753e9b28ea59f152e433ff186c8afe9fe6
Nafi Bin Sulaiman <100560842+nafibs@users.noreply.github.com>
2024-06-22 12:33:50 +0530

    Create JKFF.v

---
+++ Day 10/JKFF/JKFF.v
@@ -0,0 +1,23 @@
+module JKFF(J,K,CLK,RST,Q,Qbar);
+input J,K,CLK,RST;
+output reg Q,Qbar;
+always@(negedge CLK,negedge RST)
+if(!RST)
+begin
+if(J==0 & K==1)
+begin
+Q=0;
+Qbar =~Q;
+end
+else if(J==1 & K==0)
+begin
+Q=1;
+Qbar=~Q;
+end
+else if(J==1 & K==1)
+begin
+Q<=Qbar;
+Qbar<=Q;
+end
+end
+endmodule
