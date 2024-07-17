e80f6f3a2e95fd23d083d954092f7c80b175ed86
Nafi Bin Sulaiman <100560842+nafibs@users.noreply.github.com>
2024-07-17 13:42:23 +0530

    Update fifteencompSub_tb.v

--- Hexadecimal Compliment Subtractor/fifteencompSub_tb.v
+++ Hexadecimal Compliment Subtractor/fifteencompSub_tb.v
@@ -7,7 +7,7 @@ fifteencompSub x1(A,B,Diff,Sign);
 initial 
 begin
 A = 16'hFF04; B = 16'h2454; #10//55984
-B = 16'h00ff; A = 16'h0f21; #10//3618
+A = 16'h0f21; B = 16'h00ff;  #10//3618
 $stop;
 end
 endmodule
