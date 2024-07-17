986933ff766dd2097ca5dafdf738c64b2b4bf0a0
Nafi Bin Sulaiman <100560842+nafibs@users.noreply.github.com>
2024-07-17 13:49:53 +0530

    Create sixteencompSub_tb.v

---
+++ Hexadecimal Compliment Subtractor/sixteencompSub_tb.v
@@ -0,0 +1,14 @@
+
+module sixteencompSub_tb;
+reg [15:0]A;  
+reg [15:0]B;  
+wire [15:0]Diff;
+wire Sign;
+sixteencompSub x1(A,B,Diff,Sign);
+initial 
+begin
+A = 16'hFF04; B = 16'h2454; #10//55984
+A = 16'h0f21;B = 16'h00ff; #10//3618
+$stop;
+end
+endmodule
